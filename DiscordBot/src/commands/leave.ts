468cd6ff5902b277e08ba4d4857a168a300ab076
t11915jr <jeremy.roe@student.manchester.ac.uk>
2022-10-01 13:42:50 +0100

    Added ability to leave a party

---
+++ DiscordBot/src/commands/leave.ts
@@ -0,0 +1,60 @@
+import {ICommand} from 'wokcommands';
+import DiscordJS from 'discord.js';
+import {AppDataSource} from "../data-source";
+import {TestUser} from "../entity/TestUser";
+import {TestParty} from "../entity/TestParty";
+
+export default {
+    category: 'User',
+    description: 'To leave a campaign.',
+    slash: true,
+    // arguments
+    options: [
+        {
+            name: 'party_id',
+            description: 'The id of the party that you are trying to reject someone from.',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+        }
+    ],
+
+    callback: async ({interaction, user}) => {
+        try {
+            const userRepo = AppDataSource.getRepository(TestUser);
+            const partyRepo = AppDataSource.getRepository(TestParty);
+            const partyID = interaction.options.getNumber('party_id');
+            const find_user = await userRepo.findOne({where:{party_id:partyID, user_id:user.id}});
+            const find_party = await partyRepo.findOne({where:{id:partyID}});
+            if(!find_party){
+                await interaction.reply({
+                    content: `Party with id \`${partyID}\` could not be found`,
+                    ephemeral: true,
+                    fetchReply: true,
+                });
+                return;
+            }
+            if (find_user && find_user.status=="joined"){
+                await userRepo.delete(find_user);
+
+                await interaction.reply({
+                    content: `You have been removed from **${find_party.name}**.`,
+                    ephemeral: true,
+                    fetchReply: true,
+                });
+                return;
+            }
+            await interaction.reply({
+                content: `You could not be removed from **${find_party.name}** as are not a member of this party`,
+                ephemeral: true,
+                fetchReply: true,
+            });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({
+                content: `Something went wrong.\n${error.message} \nPlease try again :)`,
+                ephemeral: true,
+                fetchReply: true,
+            });
+        }
+    }
+} as ICommand
